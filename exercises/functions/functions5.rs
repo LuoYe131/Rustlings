c767490ac3eab55f7dd5567e61bf2d2ce6a2c59e
LuoYe131 <1990034942@qq.com>
2025-02-25 15:39:06 +0800

    update

--- exercises/functions/functions5.rs
+++ exercises/functions/functions5.rs
@@ -3,13 +3,13 @@
 // Execute `rustlings hint functions5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn main() {
     let answer = square(3);
     println!("The square of 3 is {}", answer);
 }
 
-fn square(num: i32) -> i32 {
-    num * num;
+fn square(num: i32) -> i32 {  //如何函数体中以表达式结尾且表达式没有分号，该表达式会自动被认为是函数的返回值，带分号则返回值被丢弃
+    num * num
 }
--- exercises/variables/variables4.rs
+++ exercises/variables/variables4.rs
@@ -3,10 +3,10 @@
 // Execute `rustlings hint variables4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn main() {
-    let x = 3;
+    let mut x = 3;//rust中变量默认声明为不可变immutable variable，要更改变量数据必须生命为mut
     println!("Number {}", x);
     x = 5; // don't change this line
     println!("Number {}", x);
